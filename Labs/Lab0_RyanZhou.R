3346c555873f557ad04bae0b7fad69533cae5dd2
zealerker22 <55772447+zl20144@users.noreply.github.com>
2024-01-19 11:15:09 -0500

    DA Lab 0

---
+++ Labs/Lab0_RyanZhou.R
@@ -0,0 +1,46 @@
+#Creating a dataframe
+#Example RPI Weather dataframe.
+days <- c('Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun') # days
+temp <- c(28,30.5,32,31.2,29.3,27.9, 26.4) # Temperature in F' during the winter :)
+snowed <- c('T','T','F','F','T','T','F') # Snowed on that day: T = TRUE, F = FALSE
+help("data.frame")
+RPI_Weather_Week <- data.frame(days, temp, snowed) # creating the dataframe using the data.frame() function
+
+RPI_Weather_Week
+head(RPI_Weather_Week) # head of the data frame, NOTE: it will show only 6 rows, ususally head() function shows the
+# first 6 rows of the dataframe, here we have only 7 rows in our dataframe.
+
+str(RPI_Weather_Week) # we can take a look at the structure of the dataframe using the str() function.
+
+summary(RPI_Weather_Week) # summary of the dataframe using the summaty() function
+RPI_Weather_Week[1,] # showing the 1st row and all the columns
+RPI_Weather_Week[,1] # showing the 1st column and all the rows
+
+RPI_Weather_Week[,'snowed']
+RPI_Weather_Week[,'days']
+RPI_Weather_Week[,'temp']
+RPI_Weather_Week[1:5,c("days","temp")]
+RPI_Weather_Week$temp
+subset(RPI_Weather_Week, subset=snowed==TRUE)
+
+sorted.snowed <- order(RPI_Weather_Week['snowed'])
+sorted.snowed
+RPI_Weather_Week[sorted.snowed,]
+# RPI_Weather_Week[descending_snowed,]
+dec.snow <- order(-RPI_Weather_Week$temp)
+dec.snow
+# Creating Dataframes
+# creating an empty dataframe
+empty.Dataframe <- data.frame()
+v1 <- 1:10
+v1
+letters
+v2 <- letters[1:10]
+df <- data.frame(col.name.1 = v1, col.name.2 = v2)
+df
+# importing data and exporting data
+# writing to a CSV file:
+write.csv(df, file = 'saved_df1.csv')
+df2 = read.csv('saved_df1.csv')
+df2
+
